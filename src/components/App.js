93736dcb386a747d68abdd86a35faecbeeaa3079
Gotabaya Vimukthi <gotabaya.vimukthi@gmail.com>
2023-08-26 04:19:17 +0530

    add Search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { nanoid } from 'nanoid';
-import api from "../api/Contacts"
-import Header from './Header';
-import AddContact from './AddContact';
-import ContactList from './ContactList';
-import ContactDetails from './ContactDetails';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { nanoid } from "nanoid";
+import api from "../api/Contacts";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import ContactDetails from "./ContactDetails";
 
-import './App.css';
-import EditContact from './EditContact';
+import "./App.css";
+import EditContact from "./EditContact";
 
 function App() {
-
   const [contacts, setContacts] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
 
   const retrieveContacts = async () => {
     const response = await api.get("/contacts");
     return response.data;
-  }
+  };
 
   useEffect(() => {
     // const loadContacts = JSON.parse(
@@ -29,9 +29,12 @@ function App() {
     // }
     const getContacts = async () => {
       const loadContacts = await retrieveContacts();
-      if (loadContacts) { setContacts(loadContacts)};
+      if (loadContacts) {
+        setContacts(loadContacts);
+        setSearchResult(loadContacts);
+      }
     };
-      getContacts();
+    getContacts();
   }, []);
 
   // useEffect(() => {
@@ -45,39 +48,71 @@ function App() {
     const newContact = {
       id: nanoid(),
       name: name,
-      email: email
-    }
+      email: email,
+    };
     const response = await api.post("/contacts", newContact);
     setContacts([...contacts, response.data]);
-  }
+  };
 
   const deleteContact = async (id) => {
-    await api.delete(`/contacts/${id}`)
+    await api.delete(`/contacts/${id}`);
     const newList = contacts.filter((contact) => {
       return contact.id !== id;
     });
     setContacts(newList);
-  }
+  };
 
-  const updateContact = async(contact) => {
+  const updateContact = async (contact) => {
     console.log("Update  ", contact);
-    const response = await api.put(`/contacts/${contact.id}`, contact);
+    const response = await api.put(`/contacts/${contact.id}`, ...contact);
     setContacts(
-      contacts.map((contact)=>{
-        return (contact.id === response.data.id)? {...response.data}:{contact}
+      contacts.map((contact) => {
+        return contact.id === response.data.id
+          ? { ...response.data }
+          : { contact };
       })
     );
-  }
+  };
+
+  const searchContact = (searchText) => {
+    if (searchText !== "") {
+      const result = contacts.filter((contact) => {
+        return (contact.name + contact.email)
+          .toLowerCase()
+          .includes(searchText.toLowerCase());
+      });
+      setSearchResult(result);
+    } else {
+      setSearchResult(contacts);
+    }
+  };
 
   return (
-    <div className='ui container'>
+    <div className="ui container">
       <Router>
-        <div><Header /></div>
+        <div>
+          <Header />
+        </div>
         <Routes>
-          <Route path="/" element={<ContactList contacts={contacts} handleDeleteContact={deleteContact} />} />
-          <Route path="/add" element={<AddContact handleAddContact={addContact} />} />
+          <Route
+            path="/"
+            element={
+              <ContactList
+                contacts={searchResult}
+                handleDeleteContact={deleteContact}
+                handleSearch={searchContact}
+              />
+            }
+          />
+          <Route
+            path="/add"
+            element={<AddContact handleAddContact={addContact} />}
+          />
           <Route path="/contact/:id" element={<ContactDetails />} />
-          <Route path="/edit" element={<EditContact handleUpdateContact={updateContact} />} />
+          <Route
+            path="/edit"
+            element={<EditContact handleUpdateContact={updateContact} />}
+          />
         </Routes>
       </Router>
     </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import ContactCard from './ContactCard';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ContactCard from "./ContactCard";
 
-const ContactList = ({ contacts, handleDeleteContact }) => {
+const ContactList = ({ contacts, handleDeleteContact, handleSearch }) => {
   const navigate = useNavigate();
-
   return (
-    <div className='main'>
+    <div className="main">
       <h2>
         Contact List
-        <button className='ui button blue right' onClick={() => { navigate('/add') }}>Add Contact</button>
+        <button
+          className="ui button blue right"
+          onClick={() => {
+            navigate("/add");
+          }}
+        >
+          Add Contact
+        </button>
       </h2>
-      <div className='ui called list'>
-        {contacts.map((contact) => (
-          <ContactCard contact={contact} key={contact.id} handleDeleteContact={handleDeleteContact} />
-        ))}
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+            type="text"
+            className="prompt"
+            placeholder="Search Contact"
+            onChange={(e) => {
+              handleSearch(e.target.value);
+            }}
+          />
+          <i className="search icon" />
+        </div>
+      </div>
+      <div className="ui called list">
+        {Object.keys(contacts).length > 0
+          ? contacts.map((contact) => (
+              <ContactCard
+                contact={contact}
+                key={contact.id}
+                handleDeleteContact={handleDeleteContact}
+              />
+            ))
+          : "No contacts available!"}
       </div>
     </div>
-  )
+  );
 };
 
-export default ContactList
+export default ContactList;
