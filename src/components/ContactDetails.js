3dda09c9e4bc4e912a614197930e067ce611c606
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-08-27 02:19:43 +0530

    added styles for contactDeails and contact card

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -7,18 +7,20 @@ const ContactDetails = () => {
     const navigate = useNavigate();
     const { name, email } = useLocation().state.contact;
     return (
-        <div className='contactDetails'>
-            <div className='ui card centered'>
-                <div className="image">
-                    <img src={user} alt="user" />
-                    <div className="content">
-                        <div className="header">{name}</div>
-                        <div className="description">{email}</div>
+        <div className='contactDetailsWrapper'>
+            <div className='contactDetails'>
+                <div>
+                    <div className="image">
+                        <img src={user} alt="user" />
+                        <div className="content">
+                            <div className="header">{name}</div>
+                            <div className="description">{email}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="center-div">
-                <button className="ui button blue center" onClick={() => { navigate(-1) }}>Back</button>
+                <div className="center-div">
+                    <button className="ui button blue center" onClick={() => { navigate(-1) }}>Back</button>
+                </div>
             </div>
         </div>
     );
